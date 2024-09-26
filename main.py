ff896e912466f930629e94dfff2e9a456bcd9e35
Oleg Levshov <[Dvk1bersport@yandex.ru>
2024-09-26 21:22:46 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,70 @@
+field = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+winlines = [[0, 1, 2],
+             [3, 4, 5],
+             [6, 7, 8],
+             [0, 3, 6],
+             [1, 4, 7],
+             [2, 5, 8],
+             [0, 4, 8],
+             [2, 4, 6]]
+
+
+def showfield():
+    print(field[0], end=" ")
+    print(field[1], end=" ")
+    print(field[2])
+
+    print(field[3], end=" ")
+    print(field[4], end=" ")
+    print(field[5])
+
+    print(field[6], end=" ")
+    print(field[7], end=" ")
+    print(field[8])
+
+
+def motion(move, symbol):
+    ind = field.index(move)
+    field[ind] = symbol
+
+
+def get_result():
+    win = ""
+
+    for i in winlines:
+        if field[i[0]] == "X" and field[i[1]] == "X" and field[i[2]] == "X":
+            win = "X"
+        if field[i[0]] == "O" and field[i[1]] == "O" and field[i[2]] == "O":
+            win = "O"
+
+    return win
+
+
+game_over = False
+game = True
+
+while game_over == False:
+
+    showfield()
+
+    if game == True:
+        symbol = "X"
+        move = int(input("Игрок 1, ваш ход: "))
+    else:
+        symbol = "0"
+        move = int(input("Игрок 2, ваш ход: "))
+
+    motion(move, symbol)
+    win = get_result()
+    if win != "":
+        game_over = True
+    else:
+        game_over = False
+
+    game = not (game)
+
+
+showfield()
+print("Победил", win)
+
